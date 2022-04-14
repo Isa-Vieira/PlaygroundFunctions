08d0bc175bde4bc1a0cd3e258eece1451c2bb506
Isabelly Rocha Vieira <isabellyrvieira@icloud.com>
2022-04-14 09:20:14 -0300

    requisito-6-concluido

--- src/challenges.js
+++ src/challenges.js
@@ -3,50 +3,60 @@ function compareTrue(param1, param2) {
   if (param1 === true && param2 === true) {
     return true;
   } else if (param1 === false && param2 === false) {
-  return false;
+    return false;
   } else {
-    return false; 
-  }   
+    return false;
+  }
 }
 
 // Desafio 2
 function calcArea(base, height) {
-  let area = (base * height)/ 2
+  let area = (base * height) / 2
   return area;
 }
 
 // Desafio 3
-function splitSentence(frase){
-   let resultado = frase.split(" ")
-   return resultado;
+function splitSentence(frase) {
+  let resultado = frase.split(" ")
+  return resultado;
 }
 
 
 
 // Desafio 4
 function concatName(nomes) {
-  let listanomes = nomes[nomes.length-1]
+  let listanomes = nomes[nomes.length - 1]
   let listanomes2 = nomes[0]
-   return listanomes + ', ' + listanomes2 
-} 
+  return listanomes + ', ' + listanomes2
+}
 console.log(concatName(["Ana", "Maria", "João"]))
 
 
 
 // Desafio 5
-function footballPoints(wins,ties) {
+function footballPoints(wins, ties) {
   let resultado = wins * 3 + ties * 1;
   return resultado;
 }
-console.log(footballPoints(14,8))
+console.log(footballPoints(14, 8))
 
 
 // Desafio 6
 function highestCount(arraynumeros) {
-  let numeros = [9, 1, 2, 3, 9, 5, 7];
-  return Math.max.apply(null,numeros);
-
+  let numeros = arraynumeros[0]
+  let vezesquerepete = 0
+  for(let index=0; index < arraynumeros.length; index += 1){
+    if( arraynumeros[index] > numeros)
+    numeros = arraynumeros[index]
+  }
+  for(let index=0; index < arraynumeros.length; index +=1){
+  if(arraynumeros[index] === numeros )
+      vezesquerepete += 1
+  }
+  return vezesquerepete;
 }
+console.log(highestCount([9, 1, 2, 3, 9, 5, 7]))
+
 
 // Desafio 7
 function catAndMouse() {
@@ -60,14 +70,28 @@ function fizzBuzz() {
 
 // Desafio 9
 function encode(frase) {
-  let frasecodificada = ("")
-  for(let letra of frase){
-    if(letra === 'a'){
-
+  let frasecodificada = ''
+  for (let letra of frase) {
+    if (letra == 'a') {
+      frasecodificada += '1'
+    } else if (letra =='e') {
+      frasecodificada += '2'
+    } else if (letra == 'i') {
+      frasecodificada += '3'
+    } else if (letra == '0') {
+      frasecodificada += '4'
+    } else if (letra == 'u') {
+      frasecodificada += '5'
+    } else {
+      frasecodificada = frasecodificada + letra
     }
   }
-    return frasecodificada;
+  return frasecodificada;
 }
+
+console.log(encode('frase'));
+
+
 function decode() {
   // seu código aqui
 }
